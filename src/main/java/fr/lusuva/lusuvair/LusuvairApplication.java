f2a3eaa5d03b9421d9c18f77c7bb6686128125e9
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-07-16 09:56:08 +0200

    init: Initialize project

---
+++ src/main/java/fr/lusuva/lusuvair/LusuvairApplication.java
@@ -0,0 +1,13 @@
+package fr.lusuva.lusuvair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LusuvairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LusuvairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lusuva/lusuvair/LusuvairApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lusuva.lusuvair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LusuvairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
